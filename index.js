215ce0a23f46f81aae63820151454d741a6b8f09
Rodrigo Bossini <professorbossini@gmail.com>
2024-03-09 07:56:12 -0300

    test(ap01-js): fim da secao 1.4

---
+++ index.js
@@ -0,0 +1,65 @@
+// []
+// if (null){console.log('oi')}
+// console.log([] == [])
+// console.log([] == false)
+// console.log(null == undefined)
+// console.log (null == null)
+// console.log(1 == [-1])
+// console.log(true == 1)
+// console.log( null >= 0)
+// let a = new Girafa();
+// let b = new Macaco();
+
+// let p1 = new Pessoa('V')
+// let p2 = new Pessoa('V')
+
+// if (p1 === p2)
+
+// console.log('1' === '1')
+// != !==
+// console.log(1 === '1')
+// console.log (1 === 1)
+// console.log(1 == '1')
+// console.log(1 == 1)
+
+//coerção explícita
+// const n1 = 2
+// const n2 = '3'
+// const n4 = n1 + Number(n2)
+// console.log(n4)
+
+
+//coerção implícita
+// const n3 = n1 + n2
+// console.log(n3)
+
+//  idade = 18
+// console.log(`Oi, ${nome}`)
+// let nome = 'João' //içamento (hoist)
+// if (idade >= 18){
+//   console.log(`Parabéns, ${nome}. Você pode dirigir.`)
+// }
+// console.log(`Até logo, ${nome}`)
+
+
+// var linguagem = "Javascript"
+// console.log(`Aprendendo ${linguagem}`)
+// var linguagem = "Java"
+// console.log(`Aprendendo ${linguagem}`)
+
+// var a = 2
+// a = 3
+//const, let e var
+// const idade = 19
+// console.log(idade)
+// idade = 20
+// idade.falar()
+// const nome = "José"
+// const sobrenome = 'Silva'
+// const nomeFinal = `Mendes`
+//não pode (reatribuição de constante)
+//nome = "Ana"
+// int a = 2;
+// a = "Ana";
+// a.falar();
+// String b = "abc";
