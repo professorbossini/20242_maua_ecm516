3d0ce2d388e9b3e72aa025ed142f5e13548e6594
Rodrigo Bossini <professorbossini@gmail.com>
2024-04-27 12:16:58 -0300

    chore(observacoes): inicia projeto

---
+++ mss/observacoes/src/app.ts
@@ -0,0 +1,21 @@
+let hello: string = "Hi, Typescript"
+console.log(hello)
+
+
+/*A base é uma lista
+  Uma observação é assim:
+
+  {
+    "id": um valor uuid (use o pacote uuid para isso),
+    "texto": comprar doce,
+    "lembreteId": 1
+  }
+*/
+
+//POST /lembretes/1/observacoes cadastra uma observação
+
+
+//GET /lembretes/1/observacoes obtem a lista de observacoes do lembrete 1
+
+
+
