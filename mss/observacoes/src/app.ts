4fde5b066aabfceed4a19bb01921830763638325
Rodrigo Bossini <professorbossini@gmail.com>
2024-05-04 08:34:11 -0300

    feat(observacoes): encerra o codigo 4.3.8
    - implementa get e post

--- mss/observacoes/src/app.ts
+++ mss/observacoes/src/app.ts
@@ -1,7 +1,19 @@
-let hello: string = "Hi, Typescript"
-console.log(hello)
+import dotenv from 'dotenv'
+dotenv.config()
+import express from 'express'
+import axios from 'axios'
+import { v4 as uuidv4 } from 'uuid'
+const app = express()
+app.use(express.json())
 
+const { PORT } = process.env
 
+interface Observacao{
+  id: string;
+  texto: string;
+}
+
+const base: Record <string, Observacao[]> = {}
 /*A base é uma lista
   Uma observação é assim:
 
@@ -12,10 +24,29 @@ console.log(hello)
   }
 */
 
+/*
+  {
+    "1": [
+      {id: 1, texto: oi}
+    ]
+  }
+*/
+
 //POST /lembretes/1/observacoes cadastra uma observação
-
-
+app.post('/lembretes/:id/observacoes', (req, res) => {
+  const idObs = uuidv4()
+  const { texto } = req.body
+  const observacoes: Observacao[] = base[req.params.id] || []
+  const observacao: Observacao = {id: idObs, texto}
+  observacoes.push(observacao)
+  base[req.params.id] = observacoes
+  res.status(201).json(observacao)
+})
 //GET /lembretes/1/observacoes obtem a lista de observacoes do lembrete 1
+app.get('/lembretes/:id/observacoes', (req, res) => {
+  res.status(200).json(base[req.params.id] || [])
+})
 
+app.listen(PORT, () => console.log(`Observacoes. Porta: ${PORT}.`))
 
 
