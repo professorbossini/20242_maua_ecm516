b118a011218c91c6953b2a8eeefa3a419ac6fca4
Rodrigo Bossini <professorbossini@gmail.com>
2024-05-04 09:21:26 -0300

    feat(barramento): encerra o codigo 4.3.12

---
+++ mss/barramento/src/app.ts
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import express from 'express'
+import axios from 'axios'
+const app = express()
+app.use(express.json())
+
+const { PORT } = process.env
+app.post('/eventos', function(req, res){
+  const evento = req.body
+  axios.post('http://localhost:4000/eventos', evento)
+  axios.post('http://localhost:5000/eventos', evento)
+  res.end()
+})
+
+app.listen(PORT, () => console.log(`Barramento. Porta: ${PORT}.`))
+
